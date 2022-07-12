ca9f98513d3ec4c547cb0e66ce89159cb8ac2592
Gabriel Sória <00.roomers-puzzled@icloud.com>
2022-07-12 21:10:19 +0100

    Initial Commit

---
+++ Battery/BatteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryApp.swift
+//  Battery
+//
+//  Created by Gabriel Soria Souza on 12/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct BatteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
